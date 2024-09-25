c60bf9f96dedad2ee8223bdef1cd1ea5882f8134
Ben Mao <benmao@Bens-MacBook-Air.local>
2024-09-25 18:18:33 -0400

    made code modular

--- cashier.py
+++ cashier.py
@@ -6,8 +6,26 @@ class Cashier:
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
         ###
+        """Returns the total calculated from coins inserted.
+           Hint: include input() function here, e.g. input("how many quarters?: ")"""
+        coins = {'large dollar':1, 'half dollar':0.5,'quarter': 0.25, 'nickel': 0.05}
+        total = 0
+        for coin in coins:
+            try:
+                amount = int(input(f'Insert {coin}: '))
+                total += amount * coins[coin]
+            except ValueError:
+                print(f'invalid input: {coin} try ordering again')
+        return total
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
            Hint: use the output of process_coins() function for cost input"""
-        ##
+        ##        
+        if coins < cost:
+            return False
+        else:
+            change = coins - cost
+            if change > 0:
+                print(f'Here is ${change} in change')
+            return True
--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 import data
 from sandwich_maker import SandwichMaker
 from cashier import Cashier
+from data import recipes, resources
+
 
 
 # Make an instance of other classes here
 resources = data.resources
 recipes = data.recipes
-sandwich_maker_instance = #####
-cashier_instance = ######
+sandwich_maker_instance = SandwichMaker(resources)
+cashier_instance = Cashier()
 
 
 
 
 def main():
-    ###  write the rest of the codes ###
+    ###  write the rest of the codes #### 
+    sandwich_machine = sandwich_maker_instance(resources)
+while True:
+    print("What would you like? (small/ medium/ large/ off/ report)")
+    choice = input().lower()
 
+    if choice in ["small", "medium", "large"]:
+        order_ingredients = recipes[choice]["ingredients"]
+        cost = recipes[choice]["cost"]
+        if sandwich_maker_instance.check_resources(order_ingredients):
+            coins = cashier_instance.process_coins()
+            if cashier_instance.transaction_result(coins, cost):
+                sandwich_maker_instance.make_sandwich(choice, order_ingredients)
+                print(f'{choice} sandwich is ready. Bon Appétit!')
+        else:
+            print("Out of stock")
+    elif choice == "off":
+        break
+    elif choice == "report":
+        for ingredient, quantity in sandwich_maker_instance.machine_resources.items():
+            print(f'{ingredient}: {quantity}')
 if __name__=="__main__":
     main()
+    
+
